20496ebdc9f669f0d2cc014cd7546cb09c60961f
Leo <112168257+leloa001@users.noreply.github.com>
2025-04-02 19:07:51 +0200

    Changed character script (movement now works)

--- Characters/Darryl-bartimus.gd
+++ Characters/Darryl-bartimus.gd
@@ -1,1 +1,13 @@
 extends CharacterBody2D
+
+@export var move_speed : float = 150
+
+func _physics_process(_delta):
+	var input_direction = Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), 
+		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	)
+	
+	velocity = input_direction * move_speed
+	
+	move_and_slide()
